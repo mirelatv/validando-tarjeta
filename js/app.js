1e706f103d03747353f2e07f9dc92931e517ecea
mirelatv <noelia.mirela@gmail.com>
2017-10-10 10:52:44 -0500

    Actualizando informacion de proyecto CreditCar

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
 function isvalidCar() {
-
+//solicitando informacion al usuario
     var cardNumber = prompt(" escribe el numero de tarjeta");
-
+//condicionando hasta que se cumpla las pautas requeridas
     while (cardNumber === "") {
         carNumber = prompt(" Ecribe un numero");
 
     }
+//Creando un array vacio donde se agregara el numero de tarjeta en array
     var array = [];
     for (var i = 0; i < cardNumber.length; i++) {
         array.push(parseInt(cardNumber[i].split("")));
     }
+// Inviriendo array
     var newArray = array.reverse();
-
+//Multiplicando las posiciones impares por 2
     for (var j = 0; j < newArray.length; j++) {
         if (j % 2 === 1) {
-            var duplicado = newArray[j] * 2;
-
-            newArray[j] = parseInt(duplicado / 10) + (duplicado % 10);
+            var duplicate = newArray[j] * 2;
+//sumando digitos de numeros mayores que 10
+            newArray[j] = parseInt(duplicate / 10) + (duplicate % 10);
 
         }
     }
+  // condicionando que la suma de los numeros de trajeta %10 y-especificando condiciones
+
     var SumArr = 0;
     for (var h = 0; h < newArray.length; h++) {
         SumArr += newArray[h];
